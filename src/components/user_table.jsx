f841e172e2abaff1b7a8f2578ea60c531b283adf
26dhruv <dhruvkalpeshthakkar@gmail.com>
2024-11-11 17:23:53 +0530

    Removed unnecessary button in search

--- src/components/user_table.jsx
+++ src/components/user_table.jsx
@@ -14,7 +14,6 @@ export function UserTable() {
     {/* search Bar */}
     <form className="form-inline my-2 my-lg-0">
         <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" onChange={(e)=>{setQuery(e.target.value)}}></input>
-        <button className="btn btn-outline-success my-2 my-sm-0" type="submit"  >Search</button>
       </form>
       <Link to='/AddUser'>
         <Button variant="primary" style={{ marginBottom: '10px' }}>
